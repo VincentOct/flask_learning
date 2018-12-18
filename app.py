13f93178260b9334736d919ad9abb9b991fc84c4
Vincent <bohanz0125@gmail.com>
2018-12-18 20:16:29 +0800

    demo - 5

--- app.py
+++ app.py
@@ -6,261 +6,283 @@
     :license: MIT, see LICENSE for more details.
 """
 import os
-import uuid
+import sys
 
-from flask import Flask, render_template, flash, redirect, url_for, request, send_from_directory, session
-from flask_ckeditor import CKEditor, upload_success, upload_fail
-from flask_dropzone import Dropzone
-from flask_wtf.csrf import validate_csrf
-from wtforms import ValidationError
+import click
+from flask import Flask
+from flask import redirect, url_for, abort, render_template, flash
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, TextAreaField
+from wtforms.validators import DataRequired
 
-from forms import LoginForm, FortyTwoForm, NewPostForm, UploadForm, MultiUploadForm, SigninForm, \
-    RegisterForm, SigninForm2, RegisterForm2, RichTextForm
+# sqlite URI compatible
+WIN = sys.platform.startswith('win')
+if WIN:
+    prefix = 'sqlite:///'
+else:
+    prefix = 'sqlite:////'
 
 app = Flask(__name__)
-app.secret_key = os.getenv('SECRET_KEY', 'secret string')
 app.jinja_env.trim_blocks = True
 app.jinja_env.lstrip_blocks = True
 
-# Custom config
-app.config['UPLOAD_PATH'] = os.path.join(app.root_path, 'uploads')
-app.config['ALLOWED_EXTENSIONS'] = ['png', 'jpg', 'jpeg', 'gif']
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'secret string')
 
-# Flask config
-# set request body's max length
-# app.config['MAX_CONTENT_LENGTH'] = 3 * 1024 * 1024  # 3Mb
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', prefix + os.path.join(app.root_path, 'data.db'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# Flask-CKEditor config
-app.config['CKEDITOR_SERVE_LOCAL'] = True
-app.config['CKEDITOR_FILE_UPLOADER'] = 'upload_for_ckeditor'
-
-# Flask-Dropzone config
-app.config['DROPZONE_ALLOWED_FILE_TYPE'] = 'image'
-app.config['DROPZONE_MAX_FILE_SIZE'] = 3
-app.config['DROPZONE_MAX_FILES'] = 30
-
-ckeditor = CKEditor(app)
-dropzone = Dropzone(app)
+db = SQLAlchemy(app)
 
 
-@app.route('/', methods=['GET', 'POST'])
+# handlers
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, Note=Note, Author=Author, Article=Article, Writer=Writer, Book=Book,
+                Singer=Singer, Song=Song, Citizen=Citizen, City=City, Capital=Capital,
+                Country=Country, Teacher=Teacher, Student=Student, Post=Post, Comment=Comment, Draft=Draft)
+
+
+@app.cli.command()
+@click.option('--drop', is_flag=True, help='Create after drop.')
+def initdb(drop):
+    """Initialize the database."""
+    if drop:
+        db.drop_all()
+    db.create_all()
+    click.echo('Initialized database.')
+
+
+# Forms
+class NewNoteForm(FlaskForm):
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Save')
+
+
+class EditNoteForm(FlaskForm):
+    body = TextAreaField('Body', validators=[DataRequired()])
+    submit = SubmitField('Update')
+
+
+class DeleteNoteForm(FlaskForm):
+    submit = SubmitField('Delete')
+
+
+# Models
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+
+    # optional
+    def __repr__(self):
+        return '<Note %r>' % self.body
+
+
+@app.route('/')
 def index():
-    return render_template('index.html')
+    form = DeleteNoteForm()
+    notes = Note.query.all()
+    return render_template('index.html', notes=notes, form=form)
 
 
-@app.route('/html', methods=['GET', 'POST'])
-def html():
-    form = LoginForm()
-    if request.method == 'POST':
-        username = request.form.get('username')
-        flash('Welcome home, %s!' % username)
-        return redirect(url_for('index'))
-    return render_template('pure_html.html')
-
-
-@app.route('/basic', methods=['GET', 'POST'])
-def basic():
-    form = LoginForm()
+@app.route('/new', methods=['GET', 'POST'])
+def new_note():
+    form = NewNoteForm()
     if form.validate_on_submit():
-        username = form.username.data
-        flash('Welcome home, %s!' % username)
-        return redirect(url_for('index'))
-    return render_template('basic.html', form=form)
-
-
-@app.route('/bootstrap', methods=['GET', 'POST'])
-def bootstrap():
-    form = LoginForm()
-    if form.validate_on_submit():
-        username = form.username.data
-        flash('Welcome home, %s!' % username)
-        return redirect(url_for('index'))
-    return render_template('bootstrap.html', form=form)
-
-
-@app.route('/custom-validator', methods=['GET', 'POST'])
-def custom_validator():
-    form = FortyTwoForm()
-    if form.validate_on_submit():
-        flash('Bingo!')
-        return redirect(url_for('index'))
-    return render_template('custom_validator.html', form=form)
-
-
-@app.route('/uploads/<path:filename>')
-def get_file(filename):
-    return send_from_directory(app.config['UPLOAD_PATH'], filename)
-
-
-@app.route('/uploaded-images')
-def show_images():
-    return render_template('uploaded.html')
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
-
-
-def random_filename(filename):
-    ext = os.path.splitext(filename)[1]
-    new_filename = uuid.uuid4().hex + ext
-    return new_filename
-
-
-@app.route('/upload', methods=['GET', 'POST'])
-def upload():
-    form = UploadForm()
-    if form.validate_on_submit():
-        f = form.photo.data
-        filename = random_filename(f.filename)
-        f.save(os.path.join(app.config['UPLOAD_PATH'], filename))
-        flash('Upload success.')
-        session['filenames'] = [filename]
-        return redirect(url_for('show_images'))
-    return render_template('upload.html', form=form)
-
-
-@app.route('/multi-upload', methods=['GET', 'POST'])
-def multi_upload():
-    form = MultiUploadForm()
-
-    if request.method == 'POST':
-        filenames = []
-
-        # check csrf token
-        try:
-            validate_csrf(form.csrf_token.data)
-        except ValidationError:
-            flash('CSRF token error.')
-            return redirect(url_for('multi_upload'))
-
-        # check if the post request has the file part
-        if 'photo' not in request.files:
-            flash('This field is required.')
-            return redirect(url_for('multi_upload'))
-
-        for f in request.files.getlist('photo'):
-            # if user does not select file, browser also
-            # submit a empty part without filename
-            # if f.filename == '':
-            #     flash('No selected file.')
-            #    return redirect(url_for('multi_upload'))
-            # check the file extension
-            if f and allowed_file(f.filename):
-                filename = random_filename(f.filename)
-                f.save(os.path.join(
-                    app.config['UPLOAD_PATH'], filename
-                ))
-                filenames.append(filename)
-            else:
-                flash('Invalid file type.')
-                return redirect(url_for('multi_upload'))
-        flash('Upload success.')
-        session['filenames'] = filenames
-        return redirect(url_for('show_images'))
-    return render_template('upload.html', form=form)
-
-
-@app.route('/dropzone-upload', methods=['GET', 'POST'])
-def dropzone_upload():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            return 'This field is required.', 400
-        f = request.files.get('file')
-
-        if f and allowed_file(f.filename):
-            filename = random_filename(f.filename)
-            f.save(os.path.join(
-                app.config['UPLOAD_PATH'], filename
-            ))
-        else:
-            return 'Invalid file type.', 400
-    return render_template('dropzone.html')
-
-
-@app.route('/two-submits', methods=['GET', 'POST'])
-def two_submits():
-    form = NewPostForm()
-    if form.validate_on_submit():
-        if form.save.data:
-            # save it...
-            flash('You click the "Save" button.')
-        elif form.publish.data:
-            # publish it...
-            flash('You click the "Publish" button.')
-        return redirect(url_for('index'))
-    return render_template('2submit.html', form=form)
-
-
-@app.route('/multi-form', methods=['GET', 'POST'])
-def multi_form():
-    signin_form = SigninForm()
-    register_form = RegisterForm()
-
-    if signin_form.submit1.data and signin_form.validate():
-        username = signin_form.username.data
-        flash('%s, you just submit the Signin Form.' % username)
-        return redirect(url_for('index'))
-
-    if register_form.submit2.data and register_form.validate():
-        username = register_form.username.data
-        flash('%s, you just submit the Register Form.' % username)
-        return redirect(url_for('index'))
-
-    return render_template('2form.html', signin_form=signin_form, register_form=register_form)
-
-
-@app.route('/multi-form-multi-view')
-def multi_form_multi_view():
-    signin_form = SigninForm2()
-    register_form = RegisterForm2()
-    return render_template('2form2view.html', signin_form=signin_form, register_form=register_form)
-
-
-@app.route('/handle-signin', methods=['POST'])
-def handle_signin():
-    signin_form = SigninForm2()
-    register_form = RegisterForm2()
-
-    if signin_form.validate_on_submit():
-        username = signin_form.username.data
-        flash('%s, you just submit the Signin Form.' % username)
-        return redirect(url_for('index'))
-
-    return render_template('2form2view.html', signin_form=signin_form, register_form=register_form)
-
-
-@app.route('/handle-register', methods=['POST'])
-def handle_register():
-    signin_form = SigninForm2()
-    register_form = RegisterForm2()
-
-    if register_form.validate_on_submit():
-        username = register_form.username.data
-        flash('%s, you just submit the Register Form.' % username)
-        return redirect(url_for('index'))
-    return render_template('2form2view.html', signin_form=signin_form, register_form=register_form)
-
-
-@app.route('/ckeditor', methods=['GET', 'POST'])
-def integrate_ckeditor():
-    form = RichTextForm()
-    if form.validate_on_submit():
-        title = form.title.data
         body = form.body.data
-        flash('Your post is published!')
-        return render_template('post.html', title=title, body=body)
-    return render_template('ckeditor.html', form=form)
+        note = Note(body=body)
+        db.session.add(note)
+        db.session.commit()
+        flash('Your note is saved.')
+        return redirect(url_for('index'))
+    return render_template('new_note.html', form=form)
 
 
-# handle image upload for ckeditor
-@app.route('/upload-ck', methods=['POST'])
-def upload_for_ckeditor():
-    f = request.files.get('upload')
-    if not allowed_file(f.filename):
-        return upload_fail('Image only!')
-    f.save(os.path.join(app.config['UPLOAD_PATH'], f.filename))
-    url = url_for('get_file', filename=f.filename)
-    return upload_success(url, f.filename)
+@app.route('/edit/<int:note_id>', methods=['GET', 'POST'])
+def edit_note(note_id):
+    form = EditNoteForm()
+    note = Note.query.get(note_id)
+    if form.validate_on_submit():
+        note.body = form.body.data
+        db.session.commit()
+        flash('Your note is updated.')
+        return redirect(url_for('index'))
+    form.body.data = note.body  # preset form input's value
+    return render_template('edit_note.html', form=form)
+
+
+@app.route('/delete/<int:note_id>', methods=['POST'])
+def delete_note(note_id):
+    form = DeleteNoteForm()
+    if form.validate_on_submit():
+        note = Note.query.get(note_id)
+        db.session.delete(note)
+        db.session.commit()
+        flash('Your note is deleted.')
+    else:
+        abort(400)
+    return redirect(url_for('index'))
+
+
+# one to many
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20), unique=True)
+    phone = db.Column(db.String(20))
+    articles = db.relationship('Article')  # collection
+
+    def __repr__(self):
+        return '<Author %r>' % self.name
+
+
+class Article(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), index=True)
+    body = db.Column(db.Text)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'))
+
+    def __repr__(self):
+        return '<Article %r>' % self.title
+
+
+# many to one
+class Citizen(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    city_id = db.Column(db.Integer, db.ForeignKey('city.id'))
+    city = db.relationship('City')  # scalar
+
+    def __repr__(self):
+        return '<Citizen %r>' % self.name
+
+
+class City(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+
+    def __repr__(self):
+        return '<City %r>' % self.name
+
+
+# one to one
+class Country(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    capital = db.relationship('Capital', uselist=False)  # collection -> scalar
+
+    def __repr__(self):
+        return '<Country %r>' % self.name
+
+
+class Capital(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(30), unique=True)
+    country_id = db.Column(db.Integer, db.ForeignKey('country.id'))
+    country = db.relationship('Country')  # scalar
+
+    def __repr__(self):
+        return '<Capital %r>' % self.name
+
+
+# many to many with association table
+association_table = db.Table('association',
+                             db.Column('student_id', db.Integer, db.ForeignKey('student.id')),
+                             db.Column('teacher_id', db.Integer, db.ForeignKey('teacher.id'))
+                             )
+
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    grade = db.Column(db.String(20))
+    teachers = db.relationship('Teacher',
+                               secondary=association_table,
+                               back_populates='students')  # collection
+
+    def __repr__(self):
+        return '<Student %r>' % self.name
+
+
+class Teacher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    office = db.Column(db.String(20))
+    students = db.relationship('Student',
+                               secondary=association_table,
+                               back_populates='teachers')  # collection
+
+    def __repr__(self):
+        return '<Teacher %r>' % self.name
+
+
+# one to many + bidirectional relationship
+class Writer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    books = db.relationship('Book', back_populates='writer')
+
+    def __repr__(self):
+        return '<Writer %r>' % self.name
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True)
+    writer_id = db.Column(db.Integer, db.ForeignKey('writer.id'))
+    writer = db.relationship('Writer', back_populates='books')
+
+    def __repr__(self):
+        return '<Book %r>' % self.name
+
+
+# one to many + bidirectional relationship + use backref to declare bidirectional relationship
+class Singer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(70), unique=True)
+    songs = db.relationship('Song', backref='singer')
+
+    def __repr__(self):
+        return '<Singer %r>' % self.name
+
+
+class Song(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), index=True)
+    singer_id = db.Column(db.Integer, db.ForeignKey('singer.id'))
+
+    def __repr__(self):
+        return '<Song %r>' % self.name
+
+
+# cascade
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.Text)
+    comments = db.relationship('Comment', back_populates='post', cascade='all, delete-orphan')  # collection
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    post_id = db.Column(db.Integer, db.ForeignKey('post.id'))
+    post = db.relationship('Post', back_populates='comments')  # scalar
+
+
+# event listening
+class Draft(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.Text)
+    edit_time = db.Column(db.Integer, default=0)
+
+
+@db.event.listens_for(Draft.body, 'set')
+def increment_edit_time(target, value, oldvalue, initiator):
+    if target.edit_time is not None:
+        target.edit_time += 1
+
+# same with:
+# @db.event.listens_for(Draft.body, 'set', named=True)
+# def increment_edit_time(**kwargs):
+#     if kwargs['target'].edit_time is not None:
+#         kwargs['target'].edit_time += 1
